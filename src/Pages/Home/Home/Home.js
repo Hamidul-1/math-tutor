2e4c5e46df218f3fd18100bb26c987dac4489642
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-17 02:44:13 +0600

    service completed

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,7 +3,7 @@ import Services from '../Services/Services';
 
 const Home = () => {
     return (
-        <div>
+        <div className='container'>
             <Services></Services>
         </div>
     );
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(ref => ref.json())
+            .then(data => setServices(data));
+    }, [])
+
     return (
         <div>
-            Services:
+            <h1 className='services-title'>My Services</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+
         </div>
     );
 };
